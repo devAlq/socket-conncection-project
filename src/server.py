1b49a09cf91d3240b5c230d0f7366d7264ddebd4
KhalidDev <148249284+devAlq@users.noreply.github.com>
2025-09-20 18:57:38 +0300

    Update server.py

--- src/server.py
+++ src/server.py
@@ -1,10 +1,11 @@
 import socket
 
 mySocket = socket.socket()
-mySocket.bind(("localhost", 9999))
+mySocket.bind(("localhost", 9999)) # Changed port to 8888
 mySocket.listen(3)
-while True:
-    c, address = mySocket.accept()
-    c.recv(1024).decode()
-    c.send("Hello client, your message was received successfully".encode()) # Added .encode() to send a byte string
-    c.close()
+
+c, address = mySocket.accept()
+c.recv(1024).decode()
+c.send("Hello client, your message was received successfully".encode()) # Added .encode() to send a byte string
+mySocket.close()
+c.close()
